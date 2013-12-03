6ee42fe4edab48dceb0d0f4f4bb3337c61ee0d04
Tim Krones <t.krones@gmx.net>
2013-12-03 10:41:38 +0100

    Add basic views for Ode's main components.
    Note that views for editing rules have yet to be created; we first need
    to figure out whether or not to distinguish between *creating*
    and *editing* rules.

--- ode/app/controllers/Application.java
+++ ode/app/controllers/Application.java
@@ -7,8 +7,24 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    public static Result home() {
+        return ok(home.render("Hi! This is Ode."));
+    }
+
+    public static Result rules() {
+        return ok(rules.render("Hi! This is Ode's Rule Browser."));
+    }
+
+    public static Result rule(int id) {
+        return ok(rule.render("Hi! You are looking at rule " + id + "."));
+    }
+
+    public static Result search() {
+        return ok(search.render("Hi! This is Ode's Search Interface."));
+    }
+
+    public static Result features() {
+        return ok(features.render("Hi! This is Ode's Feature Editor."));
     }
 
 }

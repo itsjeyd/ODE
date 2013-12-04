c95991be622bb428635b7f4a274d1196848c6237
Tim Krones <t.krones@gmx.net>
2013-12-04 15:04:21 +0100

    Implement high-level tests for `controllers.Secured` authenticator.
    These tests make sure that the authenticator properly handles
    authorized and unauthorized requests to protected areas of the site.

--- ode/test/controllers/AuthTest.java
+++ ode/test/controllers/AuthTest.java
@@ -13,6 +13,7 @@ import static play.test.Helpers.callAction;
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.fakeRequest;
 import static play.test.Helpers.flash;
+import static play.test.Helpers.header;
 import static play.test.Helpers.session;
 import static play.test.Helpers.status;
 
@@ -55,6 +56,72 @@ public class AuthTest extends WithApplication {
         assertNull(session(result).get("email"));
         assertThat(flash(result).get("success")).isEqualTo(
             "Alright. See you around.");
+    }
+
+    @Test
+    public void authenticated() {
+        Result rulesResult = callAction(
+            controllers.routes.ref.Application.rules(),
+            fakeRequest().withSession("email", "foo@bar.com"));
+        assertEquals(status(rulesResult), 200);
+        Result ruleResult = callAction(
+            controllers.routes.ref.Application.rule(1),
+            fakeRequest().withSession("email", "foo@bar.com"));
+        assertEquals(status(ruleResult), 200);
+        Result searchResult = callAction(
+            controllers.routes.ref.Application.search(),
+            fakeRequest().withSession("email", "foo@bar.com"));
+        assertEquals(status(searchResult), 200);
+        Result featuresResult = callAction(
+            controllers.routes.ref.Application.features(),
+            fakeRequest().withSession("email", "foo@bar.com"));
+        assertEquals(status(featuresResult), 200);
+        Result logoutResult = callAction(
+            controllers.routes.ref.Application.logout(),
+            fakeRequest().withSession("email", "foo@bar.com"));
+        assertEquals(status(logoutResult), 303);
+        assertEquals(
+            header("Location", logoutResult),
+            routes.Application.login().url());
+    }
+
+    @Test
+    public void notAuthenticated() {
+        Result rulesResult = callAction(
+            controllers.routes.ref.Application.rules(),
+            fakeRequest());
+        assertEquals(status(rulesResult), 303);
+        assertEquals(
+            header("Location", rulesResult),
+            routes.Application.login().url());
+        Result ruleResult = callAction(
+            controllers.routes.ref.Application.rule(1),
+            fakeRequest());
+        assertEquals(status(ruleResult), 303);
+        assertEquals(
+            header("Location", ruleResult),
+            routes.Application.login().url());
+        Result searchResult = callAction(
+            controllers.routes.ref.Application.search(),
+            fakeRequest());
+        assertEquals(status(searchResult), 303);
+        assertEquals(
+            header("Location", searchResult),
+            routes.Application.login().url());
+        Result featuresResult = callAction(
+            controllers.routes.ref.Application.features(),
+            fakeRequest());
+        assertEquals(status(featuresResult), 303);
+        assertEquals(
+            header("Location", featuresResult),
+            routes.Application.login().url());
+        Result logoutResult = callAction(
+            controllers.routes.ref.Application.logout(),
+            fakeRequest());
+        assertEquals(status(logoutResult), 303);
+        assertEquals(
+            header("Location", logoutResult),
+            routes.Application.login().url());
     }
 
 }

3bc9870d9d2ce02b6b18602a54bb50cd95843307
Tim Krones <t.krones@gmx.net>
2013-12-04 13:10:33 +0100

    Implement tests for authenticate action.

---
+++ ode/test/controllers/AuthTest.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Before;
+import org.junit.Test;
+import play.mvc.Result;
+import play.test.WithApplication;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static play.test.Helpers.callAction;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.session;
+import static play.test.Helpers.status;
+
+
+public class AuthTest extends WithApplication {
+
+    @Before
+    public void setUp() {
+        start(fakeApplication());
+    }
+
+    @Test
+    public void authenticateSuccess() {
+        Result result = callAction(
+            controllers.routes.ref.Application.authenticate(),
+            fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+                "email", "foo@bar.com",
+                "password", "password")));
+        assertEquals(status(result), 303);
+        assertEquals(session(result).get("email"), "foo@bar.com");
+    }
+
+    @Test
+    public void authenticateFailure() {
+        Result result = callAction(
+            controllers.routes.ref.Application.authenticate(),
+            fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+                "email", "",
+                "password", "")));
+        assertEquals(status(result), 400);
+        assertNull(session(result).get("email"));
+    }
+
+}

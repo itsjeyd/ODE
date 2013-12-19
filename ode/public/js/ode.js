43c2fcc9f9de95440dbc5a5a64e2dad9111c43a0
Tim Krones <t.krones@gmx.net>
2013-12-19 14:47:20 +0100

    Display feature values above input field for new values.

--- ode/public/js/ode.js
+++ ode/public/js/ode.js
@@ -1,32 +1,20 @@
 $(document).ready(function() {
 
-  $("#feature-form").hide();
-  $("#feature-properties").hide();
+  $("#interaction-block").hide();
+  $("#new-feature").hide();
+  $(".edit-feature").hide();
 
   $("#new-feature-button").on("click", function(event) {
     event.preventDefault();
-    $("#feature-form").show();
     $(this).hide();
-    $("#feature-properties").hide();
+    $("#interaction-block").html($("#new-feature").html());
+    $("#interaction-block").show();
   });
 
   $(".feature-item").on("click", function(event) {
-    $("#feature-form").hide();
-    $("#feature-properties h3").text($(this).text());
-    $("#feature-properties form").prop(
-      "action", "/feature/" + $(this).text());
-    $("#type-complex").removeProp("checked");
-    $("#type-atomic").removeProp("checked");
-    if ($(this).data("ftype") === "complex") {
-      $("#type-complex").prop("checked", "checked");
-      $("#permitted-values").hide();
-      $("#permitted-features").show();
-    } else {
-      $("#type-atomic").prop("checked", "checked");
-      $("#permitted-features").hide();
-      $("#permitted-values").show();
-    }
-    $("#feature-properties").show();
+    event.preventDefault();
+    $("#interaction-block").html($("#"+$(this).text()).html());
+    $("#interaction-block").show();
     $("#new-feature-button").show();
   });
 

180ce761d952c9fea7331c9a6367dbc9588b584d
Tim Krones <t.krones@gmx.net>
2014-01-03 15:29:02 +0100

    Make example integration test pass.

--- ode/test/IntegrationTest.java
+++ ode/test/IntegrationTest.java
@@ -10,15 +10,18 @@ import static org.fest.assertions.Assertions.*;
 public class IntegrationTest {
 
     /**
-     * add your integration test here
-     * in this example we just check if the welcome page is being shown
+     * Add your integration tests here.
+     * In this example we just check if the home page is being shown.
      */
     @Test
     public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+        running(
+            testServer(3333, fakeApplication(inMemoryDatabase())),
+            HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
+                assertThat(
+                    browser.pageSource()).contains("Hi! This is Ode.");
             }
         });
     }

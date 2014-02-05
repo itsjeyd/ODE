a68275f321d5a8278dfe6a1632f756a5c87fb1cc
Tim Krones <t.krones@gmx.net>
2014-02-05 18:40:19 +0100

    WIP: Refactor front-end code for Features page.
    - Create separate .js file for JS specific to "Features" page.
    - Start moving towards client-side templating with Backbone.js.

---
+++ ode/public/js/features.js
@@ -0,0 +1,103 @@
+// Models
+
+var Feature = Backbone.Model.extend({});
+var Value = Backbone.Model.extend({});
+
+var FeatureList = Backbone.Collection.extend({ model: Feature });
+var ValueList = Backbone.Collection.extend({ model: Value });
+
+
+// Model views
+
+var FeatureView = Backbone.View.extend({
+  renderListEntry: function() {
+    var listEntryTemplate = _.template(
+      '<div id="<%= name %>" class="feature-item" ' +
+        'data-name="<%= name %>" data-type="<%= type %>" ' +
+        'data-description="<%= description %>" ' +
+        'data-targets="<%= targets %>">' +
+        '<%= name %>' +
+        '</div>');
+    var attributes = this.model.toJSON();
+    return $(listEntryTemplate(attributes));
+  },
+});
+
+var ValueView = Backbone.View.extend({
+  renderListEntry: function() {
+    var listEntryTemplate = _.template(
+      '<div id="<%= name %>" class="value-item" ' +
+        'data-name="<%= name %>">' +
+        '<%= name %>' +
+        '</div>');
+    var attributes = this.model.toJSON();
+    return $(listEntryTemplate(attributes));
+  }
+});
+
+
+// Collection views
+
+var FeatureListView = Backbone.View.extend({
+  render: function() {
+    this.collection.forEach(this.addFeature, this);
+  },
+  addFeature: function(featureItem) {
+    var featureView = new FeatureView({ model: featureItem });
+    this.$el.append(featureView.renderListEntry());
+  }
+});
+
+var ValueListView = Backbone.View.extend({
+  render: function() {
+    this.collection.forEach(this.addValue, this);
+  },
+  addValue: function(valueItem) {
+    var valueView = new ValueView({ model: valueItem });
+    this.$el.append(valueView.renderListEntry());
+  }
+});
+
+
+
+$(document).ready(function() {
+
+  $('#new-feature-block').hide();
+
+  var featureItems = $('.feature-item');
+  var valueItems = $('.value-item');
+
+  var featureList = new FeatureList();
+  var valueList = new ValueList();
+
+
+  featureItems.each(function() {
+    var item = $(this);
+    var feature = new Feature({
+      name: item.data('name'),
+      type: item.data('type'),
+      description: item.data('description'),
+      targets: item.data('targets'),
+    });
+    featureList.add(feature);
+  });
+
+  valueItems.each(function() {
+    var item = $(this);
+    var value = new Value({
+      name: item.data('name'),
+    });
+    valueList.add(value);
+  });
+
+  var featureListView = new FeatureListView({
+    collection: featureList,
+    $el: $('#feature-list'),
+  });
+
+  var valueListView = new ValueListView({
+    collection: valueList,
+    $el: $('#value-list'),
+  });
+
+});

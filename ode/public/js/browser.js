644ec4e541f1db7f8b229148e5d19c495a5ff4c5
Tim Krones <t.krones@gmx.net>
2014-02-17 17:56:15 +0100

    Delete feature when clicking its `.remove-button` in Rule Browser.

--- ode/public/js/browser.js
+++ ode/public/js/browser.js
@@ -1,3 +1,11 @@
+var Rule = Backbone.Model.extend({
+
+  initialize: function() {
+    this.urlRoot = '/rules';
+  },
+
+});
+
 $(document).ready(function() {
 
   $('.rule-item').on('mouseenter', function() {
@@ -11,7 +19,12 @@ $(document).ready(function() {
       window.location.href =
         document.URL + '/' + ruleID + '/input';
     });
-
+    $('.remove-button').on('click', function(e) {
+      var ruleID = $(e.currentTarget).parents('.rule-item').attr('id')
+        .substring(1);
+      var rule = new Rule({ id: ruleID });
+      rule.destroy();
+    });
   });
 
   $('.rule-item').on('mouseleave', function() {

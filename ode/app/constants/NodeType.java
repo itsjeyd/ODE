itsjeyd/ODE

commit a1e8649162546a569172635ecaac908e9c2929a4

Implement basic back-end functionality for deleting rules.
Note that we do not check for or delete any (outgoing) relationships
yet.

ode/app/models/Rule.java
@@ -22,7 +22,7 @@ public class Rule extends LabeledNodeWithProperties {
         this.jsonProperties = Json.newObject();
     }
 
-    private Rule(String name) {
+    public Rule(String name) {
         this();
         this.name = name;
         this.jsonProperties.put("name", name);
@@ -40,6 +40,10 @@ public class Rule extends LabeledNodeWithProperties {
 
     public Promise<Boolean> create() {
         return this.exists().flatMap(new CreateFunction(this));
+    }
+
+    public Promise<Boolean> delete() {
+        return RuleManager.delete(this);
     }
 
     private static class AllFunction implements

ode/app/controllers/Rules.java
@@ -64,4 +64,20 @@ public class Rules extends Controller {
             });
     }
 
+    @Security.Authenticated(Secured.class)
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Promise<Result> delete(String name) {
+        Promise<Boolean> deleted = new Rule(name).delete();
+        return deleted.map(
+            new Function<Boolean, Result>() {
+                ObjectNode result = Json.newObject();
+                public Result apply(Boolean deleted) {
+                    if (deleted) {
+                        return ok(result);
+                    }
+                    return badRequest(result);
+                }
+            });
+    }
+
 }

ode/app/managers/RuleManager.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import play.libs.WS;
 import play.libs.F.Promise;
 
 import constants.NodeType;
 import models.Rule;
+import neo4play.Neo4jService;
+import managers.functions.NodeDeletedFunction;
 
 
 public class RuleManager extends LabeledNodeWithPropertiesManager {
@@ -19,6 +22,13 @@ public class RuleManager extends LabeledNodeWithPropertiesManager {
     public static Promise<Boolean> create(Rule rule) {
         rule.jsonProperties.put("description", rule.description);
         return LabeledNodeWithPropertiesManager.create(rule);
+    }
+
+    public static Promise<Boolean> delete(Rule rule) {
+        Promise<WS.Response> response = Neo4jService
+            .deleteLabeledNodeWithProperties(rule.label.toString(),
+                                             rule.jsonProperties);
+        return response.map(new NodeDeletedFunction());
     }
 
 }

ode/conf/routes
@@ -18,6 +18,7 @@ GET     /rules/:name/input          controllers.Rules.input(name: String)
 
 # Editing
 POST    /rules                      controllers.Rules.create()
+DELETE  /rules/:name                controllers.Rules.delete(name: String)
 
 # Search
 GET     /search                     controllers.Application.search()

commit 7ca12380b973dd67e7c1c83edce084656d5ca43e

[persistence] When adding a new rule, create a unique AVM node for representing the LHS of this rule, and connect the two nodes via an :LHS relationship.
Note that this commit breaks server-side functionality for deleting
rules (introduced by a1e8649): As explained in the associated commit
message, we do not yet check for incoming/outgoing relationships when
deleting rules, and Neo4j won't allow nodes to be deleted if they have
incoming and/or outgoing relationships.

We will most likely put off adjusting deletion behavior until
persistence functionality for LHS is completed.

ode/app/constants/NodeType.java
@@ -1,11 +1,14 @@
 package constants;
 
 public enum NodeType {
-    USER, FEATURE, VALUE, RULE;
+    USER, FEATURE, VALUE, RULE, AVM;
 
     @Override
     public String toString() {
         String name = this.name();
+        if (name.equals("AVM")) {
+            return name;
+        }
         return name.charAt(0) + name.substring(1).toLowerCase();
     }
 }

ode/app/constants/RelationshipType.java
@@ -1,5 +1,5 @@
 package constants;
 
 public enum RelationshipType {
-    ALLOWS
+    ALLOWS, LHS
 }

ode/app/managers/LHSManager.java
@@ -1,0 +1,4 @@
+package managers;
+
+
+public class LHSManager extends LabeledNodeWithPropertiesManager {}

ode/app/managers/LHSRelationshipManager.java
@@ -1,0 +1,30 @@
+package managers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import play.libs.WS;
+import play.libs.F.Promise;
+
+import models.LHSRelationship;
+import neo4play.Neo4jService;
+import managers.functions.JsonFunction;
+import managers.functions.RelationshipCreatedFunction;
+
+
+public class LHSRelationshipManager {
+
+    public static Promise<JsonNode> get(LHSRelationship relationship) {
+        Promise<WS.Response> response = Neo4jService.getTypedRelationship(
+            relationship.startNode, relationship.endNode, relationship.type);
+        return response.map(new JsonFunction());
+    }
+
+    public static Promise<Boolean> create(LHSRelationship relationship) {
+        Promise<WS.Response> response = Neo4jService
+            .createTypedRelationship(
+                relationship.startNode, relationship.endNode,
+                relationship.type);
+        return response.map(new RelationshipCreatedFunction());
+    }
+
+}

ode/app/models/LHS.java
@@ -1,0 +1,59 @@
+package models;
+
+import java.util.UUID;
+
+import play.libs.Json;
+import play.libs.F.Function;
+import play.libs.F.Promise;
+
+import constants.NodeType;
+import managers.LHSManager;
+
+
+public class LHS extends LabeledNodeWithProperties {
+    public Rule rule;
+
+    private LHS() {
+        this.label = NodeType.AVM;
+        this.jsonProperties = Json.newObject();
+    }
+
+    public LHS(Rule rule) {
+        this();
+        this.rule = rule;
+        UUID uuid = UUID.randomUUID();
+        this.jsonProperties.put("uuid", uuid.toString());
+    }
+
+    public Promise<Boolean> create() {
+        Promise<Boolean> created = this.exists()
+            .flatMap(new CreateFunction(this));
+        return created.flatMap(new ConnectToRuleFunction(this));
+    }
+
+    private class CreateFunction implements
+                                     Function<Boolean, Promise<Boolean>> {
+        private LHS lhs;
+        public CreateFunction(LHS lhs) {
+            this.lhs = lhs;
+        }
+        public Promise<Boolean> apply(Boolean exists) {
+            if (exists) {
+                return Promise.pure(false);
+            }
+            return LHSManager.create(this.lhs);
+        }
+    }
+
+    private class ConnectToRuleFunction
+        implements Function<Boolean, Promise<Boolean>> {
+        private LHS lhs;
+        public ConnectToRuleFunction(LHS lhs) {
+            this.lhs = lhs;
+        }
+        public Promise<Boolean> apply(Boolean created) {
+            return new LHSRelationship(this.lhs).create();
+        }
+    }
+
+}

ode/app/models/LHSRelationship.java
@@ -1,0 +1,50 @@
+package models;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import play.libs.F.Function;
+import play.libs.F.Promise;
+
+import constants.RelationshipType;
+import managers.LHSRelationshipManager;
+
+
+public class LHSRelationship extends TypedRelationship {
+
+    public LHSRelationship(LHS lhs) {
+        super(-1);
+        this.type = RelationshipType.LHS;
+        this.startNode = lhs.rule;
+        this.endNode = lhs;
+    }
+
+    public Promise<Boolean> exists() {
+        Promise<JsonNode> json = LHSRelationshipManager.get(this);
+        return json.map(new ExistsFunction());
+    }
+
+    public Promise<Boolean> create() {
+        return this.exists().flatMap(new CreateFunction(this));
+    }
+
+    private class ExistsFunction implements Function<JsonNode, Boolean> {
+        public Boolean apply(JsonNode json) {
+            return json.get("data").size() > 0;
+        }
+    }
+
+    private class CreateFunction implements
+                                     Function<Boolean, Promise<Boolean>> {
+        private LHSRelationship relationship;
+        public CreateFunction(LHSRelationship relationship) {
+            this.relationship = relationship;
+        }
+        public Promise<Boolean> apply(Boolean exists) {
+            if (exists) {
+                return Promise.pure(false);
+            }
+            return LHSRelationshipManager.create(this.relationship);
+        }
+    }
+
+}

ode/app/models/Rule.java
@@ -44,7 +44,9 @@ public class Rule extends LabeledNodeWithProperties {
     }
 
     public Promise<Boolean> create() {
-        return this.exists().flatMap(new CreateFunction(this));
+        Promise<Boolean> created = this.exists()
+            .flatMap(new CreateFunction(this));
+        return created.flatMap(new CreateLHSFunction(this));
     }
 
     public Promise<Boolean> updateName(final String newName) {
@@ -103,6 +105,21 @@ public class Rule extends LabeledNodeWithProperties {
                 return Promise.pure(false);
             }
             return RuleManager.create(rule);
+        }
+    }
+
+    private class CreateLHSFunction implements
+                                        Function<Boolean, Promise<Boolean>> {
+
+        private Rule rule;
+        public CreateLHSFunction(Rule rule) {
+            this.rule = rule;
+        }
+        public Promise<Boolean> apply(Boolean created) {
+            if (!created) {
+                return Promise.pure(false);
+            }
+            return new LHS(this.rule).create();
         }
     }
 }
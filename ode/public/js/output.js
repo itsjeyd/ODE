55f3d0c566046dd6e89b8cbea2f7fb5bc9690f9f
Tim Krones <t.krones@gmx.net>
2014-03-15 11:25:38 +0100

    [OutputBuilder] Add functionality for changing rule name and description to OutputBuilder.

---
+++ ode/public/js/output.js
@@ -0,0 +1,160 @@
+// Rules
+
+var Rule = Backbone.Model.extend({
+
+  initialize: function() {
+    this.urlRoot = '/rules';
+  },
+
+  _update: function(field, attrs, success) {
+    this.save(attrs,
+              { url: this.url() + '/' + field,
+                wait: true,
+                success: success,
+                error: function(model, xhr, options) {
+                  var response = $.parseJSON(xhr.responseText);
+                  model.trigger('update-error:' + field, response.message);
+                },
+              });
+  },
+
+  updateName: function(newName) {
+    var success = function(model, response, options) {
+      model.id = newName;
+    };
+    this._update('name', { name: newName }, success);
+  },
+
+  updateDescription: function(newDescription) {
+    var success = function(model, response, options) {};
+    this._update('description', { description: newDescription }, success);
+  },
+
+});
+
+var RuleView = Backbone.View.extend({
+
+  initialize: function() {
+    this.model.on({
+      'change:name': function() {
+        this._updateURL();
+      },
+      'change:description': function() {
+        this._removeEditControls('description');
+        this._renderDescription();
+      },
+      'update-error:name': function(msg) {
+        this._renderAlert('button.name', msg);
+      },
+      'update-error:description': function(msg) {
+        this._renderAlert('button.description', msg);
+      },
+    }, this);
+  },
+
+  render: function() {
+    this.$el.empty();
+    this.$el.append($.h3('').attr('id', 'rule-name'));
+    this.$el.append($.p('').attr('id', 'rule-description'));
+    this._renderName();
+    this._renderDescription();
+    return this;
+  },
+
+  _renderName: function() {
+    this.$('#rule-name').text('@' + this.model.get('name'));
+  },
+
+  _renderDescription: function() {
+    this.$('#rule-description').text(this.model.get('description'));
+  },
+
+  _updateURL: function() {
+    window.location.replace(this.model.url() + '/output');
+  },
+
+  _renderAlert: function(button, msg) {
+    var updateButton = this.$(button);
+    updateButton.next('.alert-msg').remove();
+    $.alertMsg(msg).insertAfter(updateButton);
+  },
+
+  events: {
+    'dblclick #rule-name': function(e) {
+      this._renderEditControls('name')(e)
+    },
+    'click button.name': function() {
+      this._saveEdits('name')(this)
+    },
+    'dblclick #rule-description': function(e) {
+      this._renderEditControls('description')(e)
+    },
+    'click button.description': function() {
+      this._saveEdits('description')(this)
+    },
+  },
+
+  _renderEditControls: function(modelField) {
+    return function(e) {
+      var fieldToEdit = $(e.currentTarget);
+      var currentValue = fieldToEdit.text();
+      if (modelField === 'name') {
+        currentValue = currentValue.substring(1);
+      }
+      var inputField = $.textInput().addClass(modelField)
+        .val(currentValue);
+      var okButton = $.okButton().addClass(modelField);
+      fieldToEdit.hide();
+      inputField.insertAfter(fieldToEdit);
+      okButton.insertAfter(inputField);
+      inputField.focus();
+    };
+  },
+
+  _saveEdits: function(modelField) {
+    return function(view) {
+      var inputField = view.$('input.' + modelField);
+      if (!inputField.isEmpty() &&
+          inputField.val() !== view.model.get(modelField)) {
+        var updateFunction = 'update' + $.cap(modelField);
+        view.model[updateFunction](inputField.val());
+      } else {
+        view._removeEditControls(modelField);
+        var renderFunction = '_render' + $.cap(modelField);
+        view[renderFunction]();
+      }
+    };
+  },
+
+  _removeEditControls: function(modelField) {
+    this.$('.' + modelField).remove();
+    this.$('.alert-msg').remove();
+    this.$('#rule-' + modelField).show();
+  },
+
+});
+
+
+
+// Application
+
+$(document).ready(function() {
+
+  // Output Builder
+
+  var name = $('#rule-name').text();
+  var description = $('#rule-description').text();
+
+  var rule = new Rule({
+    id: name,
+    name: name,
+    description: description,
+  });
+
+  var ruleView = new RuleView({
+    model: rule,
+    el: '#interaction-block',
+  });
+  ruleView.render();
+
+});

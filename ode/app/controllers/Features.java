itsjeyd/ODE

commit 8290a8ab20f0f1c1552e97297efe2a1c6486fea9

Refactor: Move abstract version of `create` method from AVM class to LabeledNodeWithProperties class and add implementation to subclasses inheriting from LabeledNodeWithProperties.
Note that `create` replaces `getOrCreate` in OntologyNode and User
classes.

ode/app/controllers/Features.java
@@ -78,34 +78,24 @@ public class Features extends Controller {
         final String name = json.findPath("name").textValue();
         String description = json.findPath("description").textValue();
         String type = json.findPath("type").textValue();
-        Promise<Tuple<Option<OntologyNode>, Boolean>> result = null;
+        Promise<Boolean> created = null;
         if (type.equals(FeatureType.COMPLEX.toString())) {
-            result = new ComplexFeature(name, description).getOrCreate();
+            created = new ComplexFeature(name, description).create();
         } else if (type.equals(FeatureType.ATOMIC.toString())) {
-            result = new AtomicFeature(name, description).getOrCreate();
+            created = new AtomicFeature(name, description).create();
         }
-        return result.map(
-            new Function<Tuple<Option<OntologyNode>, Boolean>, Result>() {
+        return created.map(
+            new Function<Boolean, Result>() {
                 ObjectNode jsonResult = Json.newObject();
-                public Result apply(
-                    Tuple<Option<OntologyNode>, Boolean> result) {
-                    Boolean created = result._2;
+                public Result apply(Boolean created) {
                     if (created) {
                         jsonResult.put("id", name);
                         jsonResult.put("message",
                                        "Feature successfully created.");
                         return ok(jsonResult);
-                    } else {
-                        Option<OntologyNode> feature = result._1;
-                        if (feature.isDefined()) {
-                            jsonResult.put("message",
-                                           "Feature already exists.");
-                        } else {
-                            jsonResult.put("error",
-                                           "Feature not created.");
-                        }
-                        return badRequest(jsonResult);
                     }
+                    jsonResult.put("error", "Feature not created.");
+                    return badRequest(jsonResult);
                 }
             });
     }
@@ -276,11 +266,10 @@ public class Features extends Controller {
             relationshipResult = new AllowsRelationship(feature, target)
                 .getOrCreate();
         } else if (featureType.equals(FeatureType.ATOMIC.toString())) {
-            final Feature feature = new AtomicFeature(name);
-            Promise<Tuple<Option<OntologyNode>, Boolean>> valueResult =
-                new Value(targetName).getOrCreate();
-            relationshipResult = valueResult.flatMap(
-                new MaybeConnectToValueFunction(feature));
+            Value value = new Value(targetName);
+            Promise<Boolean> created = value.create();
+            relationshipResult = created.flatMap(
+                new MaybeConnectFunction(new AtomicFeature(name), value));
         }
         return relationshipResult.map(
             new Function<Tuple<Option<Relationship>, Boolean>, Result>() {
@@ -328,19 +317,20 @@ public class Features extends Controller {
     }
 
 
-    private static class MaybeConnectToValueFunction
-        implements Function<Tuple<Option<OntologyNode>, Boolean>,
+    private static class MaybeConnectFunction
+        implements Function<Boolean,
                             Promise<Tuple<Option<Relationship>, Boolean>>> {
         private Feature feature;
-        public MaybeConnectToValueFunction(Feature feature) {
+        private Value value;
+        public MaybeConnectFunction(Feature feature, Value value) {
             this.feature = feature;
+            this.value = value;
         }
         public Promise<Tuple<Option<Relationship>, Boolean>> apply(
-            Tuple<Option<OntologyNode>, Boolean> valueResult) {
-            Option<OntologyNode> value = valueResult._1;
-            if (value.isDefined()) {
+            Boolean valueCreated) {
+            if (valueCreated) {
                 return new AllowsRelationship(
-                    feature, value.get()).getOrCreate();
+                    this.feature, this.value).getOrCreate();
             }
             return Promise.pure(
                 new Tuple<Option<Relationship>, Boolean>(

ode/app/models/nodes/OntologyNode.java
@@ -1,11 +1,7 @@
 package models.nodes;
 
 import play.libs.F.Function;
-import play.libs.F.None;
-import play.libs.F.Option;
 import play.libs.F.Promise;
-import play.libs.F.Some;
-import play.libs.F.Tuple;
 
 import constants.NodeType;
 import managers.nodes.FeatureManager;
@@ -27,51 +23,27 @@ public abstract class OntologyNode extends LabeledNodeWithProperties {
         return this.label.equals(NodeType.VALUE);
     }
 
-    public Promise<Tuple<Option<OntologyNode>, Boolean>> getOrCreate() {
-        return this.exists().flatMap(new GetOrCreateFunction(this));
+    public Promise<Boolean> create() {
+        return this.exists().flatMap(new CreateFunction(this));
     }
 
     public abstract Promise<Boolean> delete();
 
 
-    private class GetOrCreateFunction
-        implements Function<Boolean,
-                            Promise<Tuple<Option<OntologyNode>, Boolean>>> {
+    private class CreateFunction implements Function<Boolean, Promise<Boolean>> {
         private OntologyNode node;
-        public GetOrCreateFunction(OntologyNode node) {
+        public CreateFunction(OntologyNode node) {
             this.node = node;
         }
-        public Promise<Tuple<Option<OntologyNode>, Boolean>> apply(
-            Boolean exists) {
+        public Promise<Boolean> apply(Boolean exists) {
             if (exists) {
-                return Promise.pure(
-                    new Tuple<Option<OntologyNode>, Boolean>(
-                        new Some<OntologyNode>(this.node), false));
+                return Promise.pure(false);
             }
-            Promise<Boolean> created = null;
             if (this.node.isFeature()) {
-                created = FeatureManager.create((Feature) this.node);
+                return FeatureManager.create((Feature) this.node);
             } else {
-                created = ValueManager.create((Value) this.node);
+                return ValueManager.create((Value) this.node);
             }
-            return created.map(new CreatedFunction(this.node));
-        }
-    }
-
-    private class CreatedFunction
-        implements Function<Boolean, Tuple<Option<OntologyNode>, Boolean>> {
-        private OntologyNode node;
-        public CreatedFunction(OntologyNode node) {
-            this.node = node;
-        }
-        public Tuple<Option<OntologyNode>, Boolean> apply(
-            Boolean created) {
-            if (created) {
-                return new Tuple<Option<OntologyNode>, Boolean>(
-                    new Some<OntologyNode>(this.node), true);
-            }
-            return new Tuple<Option<OntologyNode>, Boolean>(
-                new None<OntologyNode>(), false);
         }
     }
 

ode/app/controllers/Auth.java
@@ -27,26 +27,16 @@ public class Auth extends Controller {
             return Promise.promise(
                 new ErrorResult(home.render(registrationForm)));
         }
-        Promise<Tuple<Option<User>, Boolean>> result = new User(
+        Promise<Boolean> created = new User(
             registrationForm.get().email,
-            registrationForm.get().password).getOrCreate();
-        return result.map(
-            new Function<Tuple<Option<User>, Boolean>, Result>() {
-                public Result apply(Tuple<Option<User>, Boolean> result) {
-                    Boolean created = result._2;
-                    // Three cases:
-                    // 1. User was created successfully: Some<User>, true
-                    // 2. User already exists: Some<User>, false
-                    // 3. User could not be created: None<User>, false
+            registrationForm.get().password).create();
+        return created.map(
+            new Function<Boolean, Result>() {
+                public Result apply(Boolean created) {
                     if (created) {
                         flash("success", "Registration successful.");
                     } else {
-                        Option<User> user = result._1;
-                        if (user.isDefined()) {
-                            flash("error", "User already exists.");
-                        } else {
-                            flash("error", "Registration failed.");
-                        }
+                        flash("error", "Registration failed.");
                     }
                     return redirect(routes.Application.home());
                 }

ode/app/models/nodes/AVM.java
@@ -32,8 +32,6 @@ public abstract class AVM extends UUIDNode {
         this.rule = rule;
     }
 
-    public abstract Promise<Boolean> create();
-
     public Promise<List<Feature>> getFeatures() {
         return HasFeatureRelationship.getEndNodes(this);
     }

ode/app/models/nodes/LabeledNodeWithProperties.java
@@ -24,4 +24,6 @@ public abstract class LabeledNodeWithProperties extends LabeledNode {
         return json.map(new ExistsFunction());
     }
 
+    public abstract Promise<Boolean> create();
+
 }

ode/app/models/nodes/Part.java
@@ -25,6 +25,10 @@ public class Part extends LabeledNodeWithProperties {
         return parts;
     }
 
+    public Promise<Boolean> create() {
+        return Promise.pure(false);
+    }
+
     private static class AllFunction
         implements Function<List<JsonNode>, List<Part>> {
         public List<Part> apply(List<JsonNode> dataNodes) {

ode/app/models/nodes/User.java
@@ -27,46 +27,12 @@ public class User extends LabeledNodeWithProperties {
         this.jsonProperties.put("username", username);
     }
 
-    public Promise<Tuple<Option<User>, Boolean>> getOrCreate() {
-        return this.exists().flatMap(new GetOrCreateFunction(this));
-    }
-
     public Promise<Option<User>> get() {
         return this.exists().map(new GetFunction(this));
     }
 
-
-    private class GetOrCreateFunction
-        implements Function<Boolean, Promise<Tuple<Option<User>, Boolean>>> {
-        private User user;
-        public GetOrCreateFunction(User user) {
-            this.user = user;
-        }
-        public Promise<Tuple<Option<User>, Boolean>> apply(Boolean exists) {
-            if (exists) {
-                return Promise.pure(
-                    new Tuple<Option<User>, Boolean>(
-                        new Some<User>(this.user), false));
-            }
-            Promise<Boolean> created = UserManager.create(this.user);
-            return created.map(new CreatedFunction(this.user));
-        }
-    }
-
-    private class CreatedFunction
-        implements Function<Boolean, Tuple<Option<User>, Boolean>> {
-        private User user;
-        public CreatedFunction(User user) {
-            this.user = user;
-        }
-        public Tuple<Option<User>, Boolean> apply(Boolean created) {
-            if (created) {
-                return new Tuple<Option<User>, Boolean>(
-                    new Some<User>(this.user), true);
-            }
-            return new Tuple<Option<User>, Boolean>(
-                new None<User>(), false);
-        }
+    public Promise<Boolean> create() {
+        return this.exists().flatMap(new CreateFunction(this));
     }
 
     private class GetFunction implements Function<Boolean, Option<User>> {
@@ -79,6 +45,19 @@ public class User extends LabeledNodeWithProperties {
                 return new Some<User>(this.user);
             }
             return new None<User>();
+        }
+    }
+
+    private class CreateFunction implements Function<Boolean, Promise<Boolean>> {
+        private User user;
+        public CreateFunction(User user) {
+            this.user = user;
+        }
+        public Promise<Boolean> apply(Boolean exists) {
+            if (exists) {
+                return Promise.pure(false);
+            }
+            return UserManager.create(this.user);
         }
     }
 

commit 67dd7dd975de8f2eed0812ce0132db00ecb421b0

Fix: Make sure *existing* values can be added to atomic features.
This feature was (semi-)broken by 8290a8a.

ode/app/controllers/Features.java
@@ -17,7 +17,6 @@ import play.libs.F.Function;
 import play.libs.F.Function0;
 import play.libs.F.Option;
 import play.libs.F.Promise;
-import play.libs.F.None;
 import play.libs.F.Tuple;
 
 import constants.FeatureType;
@@ -27,7 +26,6 @@ import models.nodes.Feature;
 import models.nodes.OntologyNode;
 import models.nodes.Value;
 import models.relationships.AllowsRelationship;
-import models.relationships.Relationship;
 
 import views.html.features;
 
@@ -258,26 +256,21 @@ public class Features extends Controller {
     private static Promise<Result> addTarget(String name, JsonNode json) {
         String featureType = json.findPath("type").textValue();
         String targetName = json.findPath("target").textValue();
-        Promise<Tuple<Option<Relationship>, Boolean>> relationshipResult =
-            null;
+        Promise<Boolean> connected = null;
         if (featureType.equals(FeatureType.COMPLEX.toString())) {
             Feature feature = new ComplexFeature(name);
             Feature target = new Feature(targetName);
-            relationshipResult = new AllowsRelationship(feature, target)
-                .getOrCreate();
+            connected = target.connectTo(feature);
         } else if (featureType.equals(FeatureType.ATOMIC.toString())) {
-            Value value = new Value(targetName);
-            Promise<Boolean> created = value.create();
-            relationshipResult = created.flatMap(
-                new MaybeConnectFunction(new AtomicFeature(name), value));
+            Feature feature = new AtomicFeature(name);
+            Value target = new Value(targetName);
+            connected = target.connectTo(feature);
         }
-        return relationshipResult.map(
-            new Function<Tuple<Option<Relationship>, Boolean>, Result>() {
+        return connected.map(
+            new Function<Boolean, Result>() {
                 ObjectNode result = Json.newObject();
-                public Result apply(
-                    Tuple<Option<Relationship>, Boolean> relationshipResult) {
-                    Boolean created = relationshipResult._2;
-                    if (created) {
+                public Result apply(Boolean connected) {
+                    if (connected) {
                         result.put("message", "Target successfully added.");
                         return ok(result);
                     }
@@ -314,28 +307,6 @@ public class Features extends Controller {
                     return badRequest(result);
                 }
             });
-    }
-
-
-    private static class MaybeConnectFunction
-        implements Function<Boolean,
-                            Promise<Tuple<Option<Relationship>, Boolean>>> {
-        private Feature feature;
-        private Value value;
-        public MaybeConnectFunction(Feature feature, Value value) {
-            this.feature = feature;
-            this.value = value;
-        }
-        public Promise<Tuple<Option<Relationship>, Boolean>> apply(
-            Boolean valueCreated) {
-            if (valueCreated) {
-                return new AllowsRelationship(
-                    this.feature, this.value).getOrCreate();
-            }
-            return Promise.pure(
-                new Tuple<Option<Relationship>, Boolean>(
-                    new None<Relationship>(), false));
-        }
     }
 
 }

ode/app/models/nodes/OntologyNode.java
@@ -6,6 +6,7 @@ import play.libs.F.Promise;
 import constants.NodeType;
 import managers.nodes.FeatureManager;
 import managers.nodes.ValueManager;
+import models.relationships.AllowsRelationship;
 
 
 public abstract class OntologyNode extends LabeledNodeWithProperties {
@@ -25,6 +26,10 @@ public abstract class OntologyNode extends LabeledNodeWithProperties {
 
     public Promise<Boolean> create() {
         return this.exists().flatMap(new CreateFunction(this));
+    }
+
+    public Promise<Boolean> connectTo(Feature feature) {
+        return new AllowsRelationship(feature, this).create();
     }
 
     public abstract Promise<Boolean> delete();

ode/app/models/nodes/Value.java
@@ -39,6 +39,29 @@ public class Value extends OntologyNode {
         return ValueManager.updateName(this, newName);
     }
 
+    @Override
+    public Promise<Boolean> connectTo(final Feature feature) {
+        Promise<Boolean> exists = this.exists();
+        return exists.flatMap(
+            new Function<Boolean, Promise<Boolean>>() {
+                public Promise<Boolean> apply(Boolean exists) {
+                    if (exists) {
+                        return Value.super.connectTo(feature);
+                    }
+                    Promise<Boolean> created = Value.this.create();
+                    return created.flatMap(
+                        new Function<Boolean, Promise<Boolean>>() {
+                            public Promise<Boolean> apply(Boolean created) {
+                                if (created) {
+                                    return Value.super.connectTo(feature);
+                                }
+                                return Promise.pure(false);
+                            }
+                        });
+                    }
+            });
+    }
+
     public Promise<Boolean> delete() {
         return ValueManager.delete(this);
     }

ode/app/models/relationships/AllowsRelationship.java
@@ -31,6 +31,24 @@ public class AllowsRelationship extends TypedRelationship {
         this.endNode = endNode;
     }
 
+    public Promise<Boolean> create() {
+        return this.exists().flatMap(new CreateFunction(this));
+    }
+
+    private class CreateFunction implements
+                                     Function<Boolean, Promise<Boolean>> {
+        private AllowsRelationship relationship;
+        public CreateFunction(AllowsRelationship relationship) {
+            this.relationship = relationship;
+        }
+        public Promise<Boolean> apply(Boolean exists) {
+            if (exists) {
+                return Promise.pure(false);
+            }
+            return AllowsRelationshipManager.create(this.relationship);
+        }
+    }
+
     public Promise<Tuple<Option<Relationship>, Boolean>> getOrCreate() {
         return this.exists().flatMap(new GetOrCreateFunction(this));
     }
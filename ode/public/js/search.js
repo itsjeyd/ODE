74593e624a2883514edcb3df1c3a57461a29eba5
Tim Krones <t.krones@gmx.net>
2014-04-22 13:16:10 +0200

    [Search] Render a single input field for "Features" and "Strings" each.

--- ode/public/js/search.js
+++ ode/public/js/search.js
@@ -17,16 +17,43 @@ var Feature = Backbone.Model.extend({
 
 // Views
 
-var SearchTargetView = Backbone.View.extend({});
+var SearchTargetView = Backbone.View.extend({
+
+  render: function() {
+    this._addFeatureField();
+    this._addStringField();
+    return this;
+  },
+
+  _addFeatureField: function() {
+    this._addSearchField('#features');
+  },
+
+  _addStringField: function() {
+    this._addSearchField('#strings');
+  },
+
+  _addSearchField: function(blockID) {
+    var searchField = $.div('search-field col-md-11');
+    searchField.append($.textInput());
+    this.$(blockID).append(searchField);
+    var controls = $.div('controls col-md-1');
+    controls.append($.plusButton());
+    this.$(blockID).append(controls);
+  },
+
+});
 
 // Application
 
 $(document).ready(function() {
 
   var searchTarget = new SearchTarget();
+
   var searchTargetView = new SearchTargetView({
     model: searchTarget,
     el: '.container-full',
   });
+  searchTargetView.render();
 
 });

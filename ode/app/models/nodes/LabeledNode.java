721b61e89d070bc3b5d29807f268e765dea350e2
Tim Krones <t.krones@gmx.net>
2014-05-31 00:59:57 +0200

    Refactor: Make `LabeledNode` class invisible to classes outside the `models.nodes` package.

--- ode/app/models/nodes/LabeledNode.java
+++ ode/app/models/nodes/LabeledNode.java
@@ -3,7 +3,7 @@ package models.nodes;
 import constants.NodeType;
 
 
-public abstract class LabeledNode extends Node {
+abstract class LabeledNode extends Node {
     public NodeType label;
 
     public String getLabel() {

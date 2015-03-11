713108d7255528aa1e4d81c87995dfd975373391
Tim Krones <t.krones@gmx.net>
2015-03-11 11:30:29 +0000

    Add copyright notice to source files.

--- ode/public/js/combinations.js
+++ ode/public/js/combinations.js
@@ -1,3 +1,20 @@
+// combinations.js --- Utilities for computing full set of output strings from a list of slots.
+
+// Copyright (C) 2013-2015  Tim Krones
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 var cart = function(slots) {
   if (slots.length === 0) {
     return [];
